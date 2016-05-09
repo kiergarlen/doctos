d395ac7c2e979aa7e052854bc898a775ca61b638
Oscar Iván González Muñoz <oscar_igm@hotmail.com>
2016-05-09 00:38:42 -0500

    Model draft

---
+++ model.js
@@ -0,0 +1,26 @@
+[
+  {
+    "_id": ObjectId("507f1f77bcf86cd799439011"),
+    "isInternal": true,
+    "hasInstructions": false,
+    "status": "Pendiente original",
+    "url": "../../archivos/escaneados/2015/05/11/131245747.pdf",
+    "reception": {
+      "control": 29067,
+      "date": ISODate("2015-05-11T01:00:00+05:00"),
+      "office": "UEAS",
+      "area": "UEAS",
+      "receptionist": "Antonio Chávez"
+    },
+    "response": {
+      "createdAt": ISODate("2015-05-13T15:36:28+05:00"),
+      "respondents": [
+        {
+          "letterNumber": "OFC. DG-001/2015",
+          "organization": "Secretaría de Infraestructura y Obra Pública",
+          "name": "Ing. roberto Dávalos López",
+        }
+      ]
+    }
+  }
+]
