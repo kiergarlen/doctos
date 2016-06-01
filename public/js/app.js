c933b005b2ec729a53993b303aaffe8c122eacc7
kiergarlen <oscar_igm@hotmail.com>
2016-06-01 18:15:38 -0500

    Minor fixes

--- public/js/app.js
+++ public/js/app.js
@@ -72,30 +72,6 @@
         // headers: {
         //   'Auth-Token': TokenService.getTokenHeader()
         // }
-      },
-      get: {
-        method: 'GET',
-        params: {},
-        isArray: true//,
-        // headers: {
-        //   'Auth-Token': TokenService.getTokenHeader()
-        // }
-      },
-      update: {
-        method: 'POST',
-        params: {},
-        isArray: false//,
-        // headers: {
-        //   'Auth-Token': TokenService.getTokenHeader()
-        // }
-      },
-      save: {
-        method: 'POST',
-        params: {},
-        isArray: false//,
-        // headers: {
-        //   'Auth-Token': TokenService.getTokenHeader()
-        // }
       }
     });
   }
