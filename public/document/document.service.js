0d955038460bab4637d67b4269413aa6dd097130
Oscar Iván González Muñoz <oscar_igm@hotmail.com>
2016-06-28 08:39:41 -0500

    Authorization regression

--- public/document/document.service.js
+++ public/document/document.service.js
@@ -13,34 +13,34 @@
       query: {
         method: 'GET',
         params: {documentId: 'documentId'},
-        isArray: false,
-        headers: {
-          'Auth-Token': TokenService.getTokenHeader()
-        }
+        isArray: false//,
+        // headers: {
+        //   'Auth-Token': TokenService.getTokenHeader()
+        // }
       },
       get: {
         method: 'GET',
         params: {},
         isArray: true,
-        headers: {
-          'Auth-Token': TokenService.getTokenHeader()
-        }
+        // headers: {
+        //   'Auth-Token': TokenService.getTokenHeader()
+        // }
       },
       update: {
         method: 'POST',
         params: {},
-        isArray: false,
-        headers: {
-          'Auth-Token': TokenService.getTokenHeader()
-        }
+        isArray: false//,
+        // headers: {
+        //   'Auth-Token': TokenService.getTokenHeader()
+        // }
       },
       save: {
         method: 'POST',
         params: {},
-        isArray: false,
-        headers: {
-          'Auth-Token': TokenService.getTokenHeader()
-        }
+        isArray: false//,
+        // headers: {
+        //   'Auth-Token': TokenService.getTokenHeader()
+        // }
       }
     });
   }
