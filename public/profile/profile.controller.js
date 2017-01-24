9439c662c96d551993cbc6ea6eb3ef42e06d4adb
kiergarlen <oscar_igm@hotmail.com>
2017-01-24 12:32:13 -0600

    Profile draft

--- public/profile/profile.controller.js
+++ public/profile/profile.controller.js
@@ -6,35 +6,43 @@
     .controller('ProfileController', ProfileController);
 
   ProfileController.$inject = [
-    '$scope', '$location', '$mdDialog'
+    '$scope',
+    '$location',
+    '$mdDialog',
+    'TokenService',
+    'PasswordService'
   ];
 
-  function ProfileController($scope, $location, $mdDialog) {
+  function ProfileController(
+      $scope,
+      $location,
+      $mdDialog,
+      TokenService,
+      PasswordService
+    ) {
     var vm = this;
-    vm.isLoggedIn = false;
-    vm.userData = {};
+    vm.fullName = TokenService.getUserFromToken()._doc.name;
     vm.currentPassword = '';
     vm.newPassword = '';
-
-    vm.submitForm = submitForm;
-
-    function submitForm(ev) {
-      //TODO: Check if password is valid
-      //Show user feddback before persisting to database
+    vm.submitForm = function (ev) {
+      // TODO: Check if password is valid
+      // Show user feddback before persisting to database
       var confirm = $mdDialog.confirm()
         .title('¿Confirmar cambio de contraseña?')
-        .textContent('Su contraseña será actualizada y saldrá del sistema')
-        .ariaLabel('Confirmar el cambio de contraseña')
+        .textContent('Se actualizará su contraseña y saldrá del sistema')
+        .ariaLabel('Confirmar cambio de contraseña')
         .targetEvent(ev)
         .ok('Aceptar')
         .cancel('Cancelar');
       $mdDialog.show(confirm)
         .then(function() {
+          //PasswordService.
           //TODO: persist changes to database
           //TODO: if persist succeeds, logout and navigate to login
           $location.path('/login');
         }, function() {
-          //TODO: clear password update form
+          vm.currentPassword = '';
+          vm.newPassword = '';
         }
       );
     }
